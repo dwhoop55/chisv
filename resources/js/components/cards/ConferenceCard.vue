3bd6a761e35f7dffe41dec623a542be7f94270ce
Florian Busch <busch@cs.rwth-aachen.de>
2019-08-15 17:01:00 +0200

    conferenceCard hover

--- resources/js/components/cards/ConferenceCard.vue
+++ resources/js/components/cards/ConferenceCard.vue
@@ -1,31 +1,33 @@
 <template>
-  <div class="card">
-    <div class="card-image is-cover" :style="backgrounStyle">
-      <!-- <figure class="image is-16by2 is-cover">
+  <a :href="conferenceUrl">
+    <div class="card is-hoverable is-clickable">
+      <div class="card-image is-cover" :style="backgrounStyle">
+        <!-- <figure class="image is-16by2 is-cover">
         <img :src="image" />
-      </figure>-->
-    </div>
-    <div class="card-content">
-      <div class="media">
-        <div class="media-left">
-          <figure class="image is-48x48">
-            <img :src="icon" />
-          </figure>
-        </div>
-        <div class="media-content">
-          <p class="title is-4">{{ conference.name | textlimit(70) }}</p>
-          <p
-            class="subtitle is-6"
-          >{{ conference.start_date | moment("ll") }} – {{ conference.end_date | moment("ll") }}</p>
-        </div>
+        </figure>-->
       </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img :src="icon" />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{{ conference.name | textlimit(70) }}</p>
+            <p
+              class="subtitle is-6"
+            >{{ conference.start_date | moment("ll") }} – {{ conference.end_date | moment("ll") }}</p>
+          </div>
+        </div>
 
-      <div class="content">
-        {{ conference.description | textlimit(300) }}
-        <br />
+        <div class="content">
+          {{ conference.description | textlimit(300) }}
+          <br />
+        </div>
       </div>
     </div>
-  </div>
+  </a>
 </template>
 
 <script>
@@ -49,6 +51,9 @@ export default {
       } else {
         return `https://avatars.dicebear.com/v2/jdenticon/${this.conference.key}.svg`;
       }
+    },
+    conferenceUrl: function() {
+      return `/conference/${this.conference.key}`;
     }
   }
 };
