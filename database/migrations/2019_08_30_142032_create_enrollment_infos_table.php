adfb64a1abb7c4c09719066bcf59d3938c529f5d
Florian Busch <busch@cs.rwth-aachen.de>
2019-09-04 15:37:17 +0200

    next fk try

---
+++ database/migrations/2019_08_30_142032_create_enrollment_infos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEnrollmentInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrollment_infos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('permission_id', false, true)->index();
+            $table->boolean('know_city')->index();
+            $table->boolean('attended_before')->index();
+            $table->boolean('sved_before')->index();
+            $table->boolean('need_visa')->index();
+            $table->text('why')->nullable();
+
+            $table->foreign('permission_id')
+                ->references('id')->on('permissions')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrollment_infos');
+    }
+}
