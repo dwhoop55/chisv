4f2865f61727f40bcd6ff15650c646d8fcd90599
Florian Busch <busch@cs.rwth-aachen.de>
2019-11-22 16:13:07 +0100

    BidPicker setup

---
+++ resources/js/components/pickers/BidPicker.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-field>
+    <b-radio-button v-model="value.preference" native-value="1">1</b-radio-button>
+    <b-radio-button v-model="value.preference" native-value="2">2</b-radio-button>
+    <b-radio-button v-model="value.preference" native-value="3">3</b-radio-button>
+    <b-radio-button v-model="value.preference" native-value="X">X</b-radio-button>
+  </b-field>
+</template>
+
+<script>
+export default {
+  props: ["value"]
+};
+</script>
