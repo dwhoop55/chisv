8ea43a3a2dc895c462edecce4bfb6405aa29c952
Florian Busch <busch@cs.rwth-aachen.de>
2020-01-09 14:38:43 +0100

    Fixed wrong tab bug when going to 'my conferences'

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="columns is-multiline is-flex-stretch">
       <div v-for="link in links" :key="link.name">
         <div class="column">
-          <a :href="link.href">
+          <a @click="click(link)">
             <div :class="cardClass(link)">
               <p>{{link.name}}</p>
             </div>
@@ -19,18 +19,25 @@ export default {
   props: ["user"],
 
   methods: {
-    cardClass: function(link) {
+    cardClass(link) {
       return `card ${link.color} notification is-hoverable-anim is-clickable is-100vh has-text-centered is-size-4 has-padding-1`;
+    },
+    click(element) {
+      if (element.store) {
+        this.$store.commit(element.store.key, element.store.value);
+      }
+      this.goTo(element.path);
     }
   },
 
   data() {
     return {
       links: [
-        { name: "All Conferences", href: "/conference", color: "is-success" },
+        { name: "All Conferences", path: "/conference", color: "is-success" },
         {
           name: "My Conferences",
-          href: `/user/${this.user.id}/edit`,
+          path: `/user/${this.user.id}/edit`,
+          store: { key: "PROFILE_TAB", value: 0 },
           color: "is-warning"
         }
       ]
