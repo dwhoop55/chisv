f383aacd6a2ed595dcf4b5f035bfde1987dfa58e
Florian Busch <busch@cs.rwth-aachen.de>
2020-03-16 19:16:38 +0100

    Removed legacy can system

--- resources/js/components/EnrollmentComponent.vue
+++ resources/js/components/EnrollmentComponent.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
     <!-- Loading -->
-    <b-notification v-if="loading" has-icon :closable="false">Loading enrollment status..</b-notification>
+    <b-notification v-if="isLoading" has-icon :closable="false">Loading enrollment status..</b-notification>
 
     <!-- Errored -->
     <transition name="slide-top-fade">
-      <b-notification v-if="!loading && errored" type="is-warning" has-icon :closable="false">
+      <b-notification v-if="!isLoading && errored" type="is-warning" has-icon :closable="false">
         <p>Your enrollment status could not be loaded.</p>
         <p>{{ errored }}</p>
-        <b-button outlined inverted type="is-warning" @click="getState();getCan()">Try again</b-button>
+        <b-button outlined inverted type="is-warning" @click="getState()">Try again</b-button>
       </b-notification>
     </transition>
 
     <!-- Not enrolled -->
     <transition name="slide-top-fade">
-      <b-notification v-if="!loading && !permission" type="is-light" has-icon :closable="false">
+      <b-notification v-if="!isLoading && !permission" type="is-light" has-icon :closable="false">
         You are not enrolled in this conference
         <p v-if="conference.state.name != 'enrollment'">
           The enrollment phase is
@@ -25,7 +25,7 @@
 
     <!-- Enrolled, Accepted, Waitlisted or Dropped -->
     <b-notification
-      v-if="!loading && permission"
+      v-if="!isLoading && permission"
       :type="stateType(permission.state)"
       has-icon
       :closable="false"
@@ -44,16 +44,16 @@
         </b-tooltip>
       </p>
       <b-button
-        v-if="!loading && permission && form"
+        v-if="!isLoading && permission && form"
         type="is-primary"
         class="has-margin-t-7"
         outlined
         icon-left="format-list-bulleted"
-        @click="showEnrollmentForm=true"
+        @click="showModal"
       >Show Enrollment Form</b-button>
       <!-- Unenroll -->
       <b-button
-        v-if="!loading && canUnenroll"
+        v-if="!isLoading && canUnenroll"
         type="is-danger"
         class="has-margin-t-7"
         outlined
@@ -61,14 +61,14 @@
         @click="confirmUnenroll"
       >Unenroll</b-button>
 
-      <p v-if="!loading && canUnenroll==false">You can no longer unenroll</p>
+      <p v-if="!isLoading && !canUnenroll">You can no longer unenroll</p>
     </b-notification>
 
     <!-- Enroll form -->
     <transition name="slide-top-fade">
       <b-collapse
         :open="true"
-        v-if="!loading && canEnroll && !permission"
+        v-if="!isLoading && canEnroll && !permission"
         class="card"
         aria-id="contentIdForEnrollForm"
       >
@@ -101,44 +101,41 @@
         </form>
       </b-collapse>
     </transition>
-
-    <b-modal :active.sync="showEnrollmentForm" has-modal-card>
-      <enrollment-form-modal v-model="form" disabled></enrollment-form-modal>
-    </b-modal>
   </div>
 </template>
 
 <script>
 import api from "@/api.js";
-import auth from "@/auth.js";
 import Form from "vform";
+import { mapGetters } from "vuex";
+import EnrollmentFormModal from "./../components/modals/EnrollmentFormModal";
 
 export default {
   props: ["conference"],
-  model: {
-    prop: "conference"
-  },
 
   data() {
     return {
       form: null,
-      showEnrollmentForm: false,
-
       permission: null,
       errored: null,
-      loading: true,
-
-      canUnenroll: null,
-      canEnroll: null
+      isLoading: true
     };
   },
 
   created() {
-    this.getCan();
     this.getState();
   },
 
   methods: {
+    showModal() {
+      this.$buefy.modal.open({
+        parent: this,
+        component: EnrollmentFormModal,
+        props: { value: this.form },
+        hasModalCard: true,
+        trapFocus: true
+      });
+    },
     confirmUnenroll: function() {
       // We warn the user and want a confirmation
       // when re-enrollment isnt possible after the user unenrolled
@@ -158,7 +155,7 @@ export default {
     },
 
     unenroll: function() {
-      this.loading = true;
+      this.isLoading = true;
       api
         .unenroll(this.conference.key)
         .then(data => {
@@ -170,13 +167,13 @@ export default {
           this.errored = error.message;
         })
         .finally(() => {
-          this.getCan();
-          this.loading = false;
+          this.getState();
+          this.isLoading = false;
         });
     },
 
     enroll: function() {
-      this.loading = true;
+      this.isLoading = true;
       api
         .enroll(this.conference.key, this.form.vform)
         .then(data => {
@@ -190,16 +187,13 @@ export default {
           }
         })
         .finally(() => {
-          this.getCan();
-          this.loading = false;
+          this.getState();
+          this.isLoading = false;
         });
     },
 
     getState: function() {
-      if (!this.conference) {
-        return;
-      }
-      this.loading = true;
+      this.isLoading = true;
       this.errored = null;
       this.permission = null;
 
@@ -229,23 +223,33 @@ export default {
           this.errored = error.message;
         })
         .finally(() => {
-          this.loading = false;
+          this.isLoading = false;
         });
-    },
-
-    getCan: async function() {
-      this.canEnroll = await auth.can(
-        "enroll",
-        "Conference",
-        this.conference.id
-      );
-
-      this.canUnenroll = await auth.can(
-        "unenroll",
-        "Conference",
-        this.conference.id
-      );
     }
+  },
+
+  computed: {
+    canEnroll() {
+      if (
+        !this.userIs("sv", this.conference.key) &&
+        this.conference.state.name === "enrollment"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    canUnenroll() {
+      if (
+        !this.userIs("sv", this.conference.key, "dropped") &&
+        this.conference.state.name != "over"
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    ...mapGetters("auth", ["userIs"])
   }
 };
 </script>
