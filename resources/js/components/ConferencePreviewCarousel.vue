a3d739de1e8436a64cb1a2110cf4031e4564bc7e
Florian Busch <busch@cs.rwth-aachen.de>
2020-10-19 00:46:05 +0200

    Bringing a sketch view to the conf preview slider for loading

--- resources/js/components/ConferencePreviewCarousel.vue
+++ resources/js/components/ConferencePreviewCarousel.vue
@@ -1,35 +1,39 @@
 <template>
-  <b-carousel
-    :pause-info="false"
-    :arrow="false"
-    :interval="3000"
-    indicator-position="is-top"
-    indicator-inside
-  >
-    <b-loading :is-full-page="false" :active="isLoading"></b-loading>
-    <b-carousel-item v-for="(conference, index) in conferences" :key="index">
-      <div class="is-100vh">
-        <div
-          class="card-image is-cover"
-          :style="`height:350px;${conferenceArtworkBackground(conference)}`"
-        >
+  <div>
+    <b-skeleton height="350px" size="is-large" :active="isLoading"></b-skeleton>
+    <b-carousel
+      v-show="!isLoading"
+      :pause-info="false"
+      :arrow="false"
+      :interval="3000"
+      indicator-position="is-top"
+      indicator-inside
+    >
+      <b-carousel-item v-if="isLoading"> </b-carousel-item>
+      <b-carousel-item v-for="(conference, index) in conferences" :key="index">
+        <div class="is-100vh">
           <div
-            :style="stateBackground(conference.state)"
-            class="has-text-centered has-text-weight-bold has-text-shadow is-rounded-b-l is-rounded-b-r is-pinned-t is-pinned-l has-text-white-bis is-absolute has-margin-l-7 has-padding-r-5 has-padding-l-5 has-padding-b-7 has-padding-t-7"
+            class="card-image is-cover"
+            :style="`height:350px;${conferenceArtworkBackground(conference)}`"
           >
-            <p>{{ conference.name }}</p>
-            <p>
-              {{
-                conference.state && conference.state.name
-                  ? conference.state.name
-                  : "Unknown status" | capitalize
-              }}
-            </p>
+            <div
+              :style="stateBackground(conference.state)"
+              class="has-text-centered has-text-weight-bold has-text-shadow is-rounded-b-l is-rounded-b-r is-pinned-t is-pinned-l has-text-white-bis is-absolute has-margin-l-7 has-padding-r-5 has-padding-l-5 has-padding-b-7 has-padding-t-7"
+            >
+              <p>{{ conference.name | textlimit(30) }}</p>
+              <p>
+                {{
+                  conference.state && conference.state.name
+                    ? conference.state.name
+                    : "Unknown status" | capitalize
+                }}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </b-carousel-item>
-  </b-carousel>
+      </b-carousel-item>
+    </b-carousel>
+  </div>
 </template>
 
 <script>
@@ -39,11 +43,7 @@ export default {
   data() {
     return {
       isLoading: true,
-      conferences: [
-        {
-          name: "Loading",
-        },
-      ],
+      conferences: null,
     };
   },
 
