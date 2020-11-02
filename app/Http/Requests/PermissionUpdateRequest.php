3dbbbdbd78b9091af984a16dbd0727045f5b941f
Florian Busch <busch@cs.rwth-aachen.de>
2020-11-02 17:44:08 +0100

    Streamlined permission state update

--- app/Http/Requests/PermissionUpdateRequest.php
+++ app/Http/Requests/PermissionUpdateRequest.php
@@ -16,13 +16,7 @@ class PermissionUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'id' => [
-                'required',
-                'exists:permissions,id',
-            ],
             'state_id' => [
-                'nullable',
-                'required_if:role_id,' . Role::byName('sv'),
                 'exists:states,id',
                 'gte:10',
                 'lt:20',
@@ -33,16 +27,7 @@ class PermissionUpdateRequest extends FormRequest
     public function messages()
     {
         return [
-            'id.required' => "You need to specify which permission",
             'state_id.*' => 'Invalid state id',
-        ];
-    }
-
-    public function attributes()
-    {
-        return [
-            'user_id' => 'user',
-            'state_id' => 'state',
         ];
     }
 }
